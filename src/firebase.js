2f924875aee57b8ea8fb2858be9c1565ed3d933c
gamaxhakorsar <145942508+gamaxhakorsar@users.noreply.github.com>
2023-09-27 03:11:37 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB91DyrJQBNG1q7p4Elxvi3ptgoIMYV_ps",
-  authDomain: "myfriend-73ba6.firebaseapp.com",
-  projectId: "myfriend-73ba6",
-  storageBucket: "myfriend-73ba6.appspot.com",
-  messagingSenderId: "317325494743",
-  appId: "1:317325494743:web:8a67a63fe86920718de392",
-  measurementId: "G-P3VJRMQWCQ"
+  apiKey: "AIzaSyCOmTnd8Md2yK0SVEwI1l6VbV1M7N8ySaA",
+  authDomain: "magneti1001.firebaseapp.com",
+  projectId: "magneti1001",
+  storageBucket: "magneti1001.appspot.com",
+  messagingSenderId: "596941070186",
+  appId: "1:596941070186:web:e9af3e1e5ecb7459e80f13",
+  measurementId: "G-Z02PPN0HRS"
 };
 // Initialize Firebase
 
